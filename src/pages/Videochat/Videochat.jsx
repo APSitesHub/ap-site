b75dc32bba209c75cd1cf9c31ed79d295cd5f4f3
Olexandr Gordijchuk <san4ez1717@gmail.com>
2025-03-06 12:24:03 +0200

    add slug to room

--- src/pages/Videochat/Videochat.jsx
+++ src/pages/Videochat/Videochat.jsx
@@ -3,18 +3,40 @@ import { useNavigate } from 'react-router-dom';
 import { getRooms } from './utils/api/getRooms';
 import { createRoom } from './utils/api/createRoom';
 import { getToken } from './utils/api/getToken';
-import { Container, Title, SubTitle, Label, Input, Button, RoomList, RoomItem, JoinButton } from './Videochat.styled';
+import {
+  Container,
+  Title,
+  SubTitle,
+  FormGroup,
+  Label,
+  Input,
+  Select,
+  Button,
+  RoomList,
+  RoomItem,
+  JoinButton,
+} from './Videochat.styled';
+import { getKahoots } from './utils/api/getKahoots';
 
 function Videochat() {
   const navigate = useNavigate();
   const [rooms, setRooms] = useState([]);
+  const [roomLevels, setRoomLevels] = useState([]);
   const newRoomName = useRef('');
+  const newRoomLevel = useRef(null);
 
   useEffect(() => {
     if (!getToken()) {
       navigate('../teacher-login');
     }
 
+    const updateRoomLevels = async () => {
+      const levels = await getKahoots();
+
+      setRoomLevels(levels);
+    };
+
+    updateRoomLevels();
     updateRooms();
     // eslint-disable-next-line
   }, []);
@@ -26,8 +48,7 @@ function Videochat() {
   };
 
   const handleCreateRoom = async () => {
-    await createRoom(newRoomName.current.value);
-
+    await createRoom(newRoomName.current.value, newRoomLevel.current.value);
     await updateRooms();
   };
 
@@ -39,8 +60,20 @@ function Videochat() {
         <SubTitle>Create Room</SubTitle>
       </div>
 
-      <Label htmlFor="room-name">Room name:</Label>
-      <Input type="text" id="room-name" ref={newRoomName} />
+      <FormGroup>
+        <Label htmlFor="room-name">Room name:</Label>
+        <Input type="text" id="room-name" ref={newRoomName} />
+      </FormGroup>
+      <FormGroup>
+        <Label htmlFor="room-level">Room level:</Label>
+        <Select id="room-level" ref={newRoomLevel}>
+          {roomLevels.map(level => (
+            <option key={level} value={level}>
+              {level}
+            </option>
+          ))}
+        </Select>
+      </FormGroup>
 
       <Button onClick={handleCreateRoom}>Create New Room</Button>
 
