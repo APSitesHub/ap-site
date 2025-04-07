2e08c09435ec4da8ed432016bb412f7c88a7d18a
Kyrylo Sulihan <paulhighrow@gmail.com>
2025-04-07 13:58:49 +0300

    wrong link for deb1_1 fix, toggle course button added to timetable

--- src/pages/MyAP/Timetable.jsx/Timetable.jsx
+++ src/pages/MyAP/Timetable.jsx/Timetable.jsx
@@ -1,6 +1,9 @@
-import { useRef } from 'react';
 import eyesImg from '../../../img/quiz/eyes.png';
+import de from '../../../img/svg/myap/langs/de.png';
+import en from '../../../img/svg/myap/langs/en.png';
+import pl from '../../../img/svg/myap/langs/pl.png';
 import { CalendarIcon } from '../Attendance/Attendance.styled';
+import { LangIcon } from '../MyAPPanel/MyAPPanel.styled';
 import {
   EyesEmoji,
   PointsPlaceHolder,
@@ -9,6 +12,9 @@ import {
 import {
   TimetableBody,
   TimetableBox,
+  TimetableChangeCourseBox,
+  TimetableChangeCourseBtn,
+  TimetableCourseMemo,
   TimetableDaysCell,
   TimetableDaysItem,
   TimetableHead,
@@ -31,8 +37,18 @@ import {
 //   'pro',
 // ];
 
-export const Timetable = ({ user, course, language, timetable, isMultipleLanguages }) => {
-  const userPackage = useRef(user.package === undefined ? 'pro' : user.package);
+export const Timetable = ({
+  user,
+  course,
+  language,
+  timetable,
+  setCourse,
+  courseIndex,
+  setCourseIndex,
+  isMultipleCourses,
+  isMultipleLanguages,
+}) => {
+  // const userPackage = useRef(user.package === undefined ? 'pro' : user.package);
   const personalTimetable = timetable.find(timeline =>
     language === 'enkids' && user.knowledge.includes('beginner')
       ? timeline.lang === language &&
@@ -44,17 +60,13 @@ export const Timetable = ({ user, course, language, timetable, isMultipleLanguag
   const getLink = () => {
     const baseStreamUrl = 'https://academy.ap.education/streams/';
     const baseKidsStreamUrl = 'https://academy.ap.education/streams-kids/';
-    console.log(user.lang);
-    console.log(language);
-    console.log(user.adult === undefined ? true : user.adult);
-    console.log(user.knowledge);
-    console.log(user.package === undefined ? 'student' : user.package);
-    console.log(userPackage.current);
 
     return language === 'en'
       ? baseStreamUrl + personalTimetable?.level
       : language === 'enkids'
       ? baseKidsStreamUrl + personalTimetable?.level
+      : language === 'de' && personalTimetable?.level.includes('b1')
+      ? baseStreamUrl + 'deutschb1'
       : language === 'de' && personalTimetable?.level !== 'a1'
       ? baseStreamUrl + 'deutsch' + personalTimetable?.level
       : language === 'de' && personalTimetable?.level === 'a1'
@@ -78,31 +90,8 @@ export const Timetable = ({ user, course, language, timetable, isMultipleLanguag
       ? baseKidsStreamUrl + personalTimetable?.level + 'sc'
       : baseStreamUrl + language + personalTimetable?.level + 'sc';
   };
-  const getIndividualLink = () => {
-    const enUrl = 'https://n1313568.alteg.io';
-    const enKidsUrl = 'https://n1313571.alteg.io/';
-    const deUrl = 'https://n1313569.alteg.io';
-    const deKidsUrl = 'https://n1313572.alteg.io/';
-    const plUrl = 'https://n1313570.alteg.io';
-    const plKidsUrl = 'https://n1313573.alteg.io/';
-    return language === 'en'
-      ? enUrl
-      : language === 'enkids'
-      ? enKidsUrl
-      : language === 'de'
-      ? deUrl
-      : language === 'dekids'
-      ? deKidsUrl
-      : language === 'pl'
-      ? plUrl
-      : language === 'plkids'
-      ? plKidsUrl
-      : enUrl;
-  };
 
   const link = getLink();
-  const indLink = getIndividualLink();
-  console.log(indLink);
   const speakingLink = getSpeakingLink();
 
   const DAYS = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Нд'];
@@ -113,6 +102,36 @@ export const Timetable = ({ user, course, language, timetable, isMultipleLanguag
         <CalendarIcon />
         Графік занять
       </TimetableHeading>
+      {isMultipleCourses && !isMultipleLanguages && (
+        <TimetableChangeCourseBox>
+          <TimetableCourseMemo>
+            <LangIcon
+              src={language.includes('de') ? de : language.includes('pl') ? pl : en}
+            />
+            {personalTimetable?.level}
+          </TimetableCourseMemo>
+          <TimetableChangeCourseBtn
+            className="my-ap-change-course"
+            onClick={() => {
+              setCourse(
+                course =>
+                  (course =
+                    courseIndex + 1 < user.course.split('/').length
+                      ? user.course.split('/')[courseIndex + 1]
+                      : user.course.split('/')[0])
+              );
+              setCourseIndex(
+                index =>
+                  (index = index + 1 < user.course.split('/').length ? index + 1 : 0)
+              );
+            }}
+          >
+            <TimetableLessonLinkText className="my-ap-change-course">
+              Змінити курс
+            </TimetableLessonLinkText>
+          </TimetableChangeCourseBtn>
+        </TimetableChangeCourseBox>
+      )}
       {!personalTimetable ? (
         <PointsPlaceHolder>
           <EyesEmoji src={eyesImg} alt="Eyes emoji" width="80" />
@@ -124,12 +143,63 @@ export const Timetable = ({ user, course, language, timetable, isMultipleLanguag
           </PointsPlaceHolderText>
         </PointsPlaceHolder>
       ) : (
-        <TimetableBody>
-          {!user.package.includes('sc') && (
-            <TimetableWebinars>
+        <>
+          <TimetableBody>
+            {!user.package.includes('sc') && (
+              <TimetableWebinars>
+                <TimetableWebinarsHead>
+                  <TimetableLessonType>Теоретичні заняття</TimetableLessonType>
+                  <TimetableLessonLink href={link} target="_blank">
+                    <TimetableLessonLinkText>Перейти</TimetableLessonLinkText>
+                  </TimetableLessonLink>
+                </TimetableWebinarsHead>
+                <TimetableTable>
+                  <thead>
+                    <tr>
+                      <TimetableHead className="day">День</TimetableHead>
+                      <TimetableHead className="time">Час</TimetableHead>
+                      <TimetableHead className="lessonNumber">№ уроку</TimetableHead>
+                      <TimetableHead className="teacher">Викладач</TimetableHead>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {personalTimetable.schedule
+                      .filter(
+                        lesson =>
+                          lesson.type === 'webinar' || lesson.type === 'webinar, repeat'
+                      )
+                      .sort((a, b) => a.day - b.day)
+                      .map((lesson, i) => (
+                        <TimetableDaysItem
+                          key={i}
+                          style={
+                            lesson.day === new Date().getDay()
+                              ? { backgroundColor: '#F9C838' }
+                              : {}
+                          }
+                        >
+                          <TimetableDaysCell className="day">
+                            {DAYS[lesson.day - 1]}
+                          </TimetableDaysCell>
+                          <TimetableDaysCell className="time">
+                            {lesson.time}
+                          </TimetableDaysCell>
+                          <TimetableDaysCell className="lessonNumber">
+                            {lesson.lessonNumber}
+                          </TimetableDaysCell>
+                          <TimetableDaysCell className="teacher">
+                            {lesson.teacher}
+                          </TimetableDaysCell>
+                        </TimetableDaysItem>
+                      ))}
+                  </tbody>
+                </TimetableTable>
+              </TimetableWebinars>
+            )}
+            <TimetableSpeakings>
               <TimetableWebinarsHead>
-                <TimetableLessonType>Теоретичні заняття</TimetableLessonType>
-                <TimetableLessonLink href={link} target="_blank">
+                <TimetableLessonType>Практичні заняття</TimetableLessonType>
+                <TimetableLessonLink href={speakingLink} target="_blank">
                   <TimetableLessonLinkText>Перейти</TimetableLessonLinkText>
                 </TimetableLessonLink>
               </TimetableWebinarsHead>
@@ -144,10 +214,7 @@ export const Timetable = ({ user, course, language, timetable, isMultipleLanguag
                 </thead>
                 <tbody>
                   {personalTimetable.schedule
-                    .filter(
-                      lesson =>
-                        lesson.type === 'webinar' || lesson.type === 'webinar, repeat'
-                    )
+                    .filter(lesson => lesson.type === 'speaking')
                     .sort((a, b) => a.day - b.day)
                     .map((lesson, i) => (
                       <TimetableDaysItem
@@ -174,55 +241,8 @@ export const Timetable = ({ user, course, language, timetable, isMultipleLanguag
                     ))}
                 </tbody>
               </TimetableTable>
-            </TimetableWebinars>
-          )}
-          <TimetableSpeakings>
-            <TimetableWebinarsHead>
-              <TimetableLessonType>Практичні заняття</TimetableLessonType>
-              <TimetableLessonLink href={speakingLink} target="_blank">
-                <TimetableLessonLinkText>Перейти</TimetableLessonLinkText>
-              </TimetableLessonLink>
-            </TimetableWebinarsHead>
-            <TimetableTable>
-              <thead>
-                <tr>
-                  <TimetableHead className="day">День</TimetableHead>
-                  <TimetableHead className="time">Час</TimetableHead>
-                  <TimetableHead className="lessonNumber">№ уроку</TimetableHead>
-                  <TimetableHead className="teacher">Викладач</TimetableHead>
-                </tr>
-              </thead>
-              <tbody>
-                {personalTimetable.schedule
-                  .filter(lesson => lesson.type === 'speaking')
-                  .sort((a, b) => a.day - b.day)
-                  .map((lesson, i) => (
-                    <TimetableDaysItem
-                      key={i}
-                      style={
-                        lesson.day === new Date().getDay()
-                          ? { backgroundColor: '#F9C838' }
-                          : {}
-                      }
-                    >
-                      <TimetableDaysCell className="day">
-                        {DAYS[lesson.day - 1]}
-                      </TimetableDaysCell>
-                      <TimetableDaysCell className="time">
-                        {lesson.time}
-                      </TimetableDaysCell>
-                      <TimetableDaysCell className="lessonNumber">
-                        {lesson.lessonNumber}
-                      </TimetableDaysCell>
-                      <TimetableDaysCell className="teacher">
-                        {lesson.teacher}
-                      </TimetableDaysCell>
-                    </TimetableDaysItem>
-                  ))}
-              </tbody>
-            </TimetableTable>
-          </TimetableSpeakings>
-          {/* {userPackage.current !== 'simple' && userPackage.current !== 'student' && (
+            </TimetableSpeakings>
+            {/* {userPackage.current !== 'simple' && userPackage.current !== 'student' && (
             <TimetableSpeakings>
               <TimetableWebinarsHead>
                 <TimetableLessonType>Індивідуальні заняття</TimetableLessonType>
@@ -232,7 +252,8 @@ export const Timetable = ({ user, course, language, timetable, isMultipleLanguag
               </TimetableWebinarsHead>
             </TimetableSpeakings>
           )} */}
-        </TimetableBody>
+          </TimetableBody>
+        </>
       )}
     </TimetableBox>
   );
