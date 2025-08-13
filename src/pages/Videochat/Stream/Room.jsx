cd238bc4e3b4a5969ef220338a5790ad234017f8
Olexandr Gordijchuk <san4ez1717@gmail.com>
2025-08-13 12:05:45 +0300

    full screen function updated

--- src/pages/Videochat/Stream/Room.jsx
+++ src/pages/Videochat/Stream/Room.jsx
@@ -92,10 +92,33 @@ function Room({ isAdmin }) {
   };
 
   const toggleFullScreen = () => {
-    if (document.fullscreenElement) {
-      document.exitFullscreen();
+    const element = document.documentElement;
+
+    if (
+      document.fullscreenElement ||
+      document.webkitFullscreenElement ||
+      document.mozFullScreenElement ||
+      document.msFullscreenElement
+    ) {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+      } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+      } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+      }
     } else {
-      document.documentElement.requestFullscreen();
+      if (element.requestFullscreen) {
+        element.requestFullscreen();
+      } else if (element.webkitRequestFullscreen) {
+        element.webkitRequestFullscreen();
+      } else if (element.mozRequestFullScreen) {
+        element.mozRequestFullScreen();
+      } else if (element.msRequestFullscreen) {
+        element.msRequestFullscreen();
+      }
     }
   };
 
--- src/pages/Videochat/Trial/Room.jsx
+++ src/pages/Videochat/Trial/Room.jsx
@@ -91,10 +91,33 @@ function Room({ isAdmin, lang }) {
   };
 
   const toggleFullScreen = () => {
-    if (document.fullscreenElement) {
-      document.exitFullscreen();
+    const element = document.documentElement;
+
+    if (
+      document.fullscreenElement ||
+      document.webkitFullscreenElement ||
+      document.mozFullScreenElement ||
+      document.msFullscreenElement
+    ) {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+      } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+      } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+      }
     } else {
-      document.documentElement.requestFullscreen();
+      if (element.requestFullscreen) {
+        element.requestFullscreen();
+      } else if (element.webkitRequestFullscreen) {
+        element.webkitRequestFullscreen();
+      } else if (element.mozRequestFullScreen) {
+        element.mozRequestFullScreen();
+      } else if (element.msRequestFullscreen) {
+        element.msRequestFullscreen();
+      }
     }
   };
 
